0cfc08a27e7a87644a0388a3d31d891d3afa84c6
Ray Cai <kkppccdd@gmail.com>
2017-05-27 22:46:42 +0800

    init ui prototype of custom collection management

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MdListModule, MdGridListModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CustomCollectionListComponent } from './custom-collection-list/custom-collection-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CustomCollectionListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdListModule,
+    MdGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/custom-collection-list/custom-collection-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-collection-list',
+  templateUrl: './custom-collection-list.component.html',
+  styleUrls: ['./custom-collection-list.component.css']
+})
+export class CustomCollectionListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
