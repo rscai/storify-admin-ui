5ae2d9cdb527f7c36eb58c7cff26dfd34a536d1b
Ray Cai <kkppccdd@gmail.com>
2017-05-28 12:00:41 +0800

    init service and injector

---
+++ src/app/CustomCollections.service.ts
@@ -0,0 +1,26 @@
+export class CustomCollectionsService {
+  fetch(){
+    return [
+      {
+        id:"cc_1",
+        title:"Collection1"
+      },
+      {
+        id:"cc_2",
+        title:"Collection2"
+      },
+      {
+        id:"cc_3",
+        title:"Collection3"
+      },
+      {
+        id:"cc_4",
+        title:"Collection4"
+      },
+      {
+        id:"cc_5",
+        title:"Collection5"
+      },
+    ]
+  }
+}
--- src/app/custom-collection-list/custom-collection-list.component.ts
+++ src/app/custom-collection-list/custom-collection-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CustomCollectionsService } from '../CustomCollections.service'
+
 @Component({
   selector: 'app-custom-collection-list',
   templateUrl: './custom-collection-list.component.html',
-  styleUrls: ['./custom-collection-list.component.css']
+  styleUrls: ['./custom-collection-list.component.css'],
+  providers: [CustomCollectionsService]
 })
 export class CustomCollectionListComponent implements OnInit {
-
-  constructor() { }
+  customCollectionsList: any[]
+  constructor(private customCollectionsService: CustomCollectionsService) { }
 
   ngOnInit() {
+    this.customCollectionsList = this.customCollectionsService.fetch();
   }
 
 }
